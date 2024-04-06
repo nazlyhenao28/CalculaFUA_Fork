ee7d4ace09269648964d6c1097ede9a96c43734d
JohanAlexisT <166188707+JohanAlexisT@users.noreply.github.com>
2024-04-05 21:22:48 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,11 @@
 def suma(a,b):
   return a+b
+
+def factorial(numero):
+    if numero <= 0:
+        return 1
+    factorial = 1
+    while numero > 0:
+        factorial = factorial * numero
+        numero -= 1
+    return factorial
